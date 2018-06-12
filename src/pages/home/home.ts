cfa9d5e9d83559f641a11e6a3ac6a282a93d2b4c
Jonathan Caiza <jpaul.snp@gmail.com>
2018-06-12 16:02:28 -0500

    formularios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nombre = 'Holaa';
+  password = "";
 
+  constructor(public navCtrl: NavController) {
+    // console.log('Holaaa');
   }
+
+  validarFormulario(formulario: NgForm){
+    console.log(formulario);
+  }
+
 
 }
